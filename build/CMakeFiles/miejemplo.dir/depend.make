bd3e28943f1a512e1ddc349b5c07e1a5ef884b0c
Usuario generico alumno <alumno@ubuntu2204.localdomain>
2024-06-11 08:46:50 +0200

    Añadidos cambios cmake

---
+++ build/CMakeFiles/miejemplo.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for miejemplo.
+# This may be replaced when dependencies are built.
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hola mundo" << std::endl;
+  int a = 5;
+  std::cout << a << std::endl;
+  std::cout << "Esto es nuevo\n";
+  return 0;
+}
